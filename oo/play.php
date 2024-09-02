291deb64d208b0e646ea3be913bb52b7f1abfc21
Kacper <kacper.tw@wp.pl>
2024-09-02 10:49:16 +0200

    ep2c1

--- oo/play.php
+++ oo/play.php
@@ -1,51 +1,35 @@
 <?php
 
-class Ship{
-    public $name;
-    public $weaponPower = 0;
-    public $jediFactor = 0;
-    public $strength = 0;
-
-    public function sayHello()
-    {
-        echo 'Hello!';
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getNameAndSpecs($useShortFormat)
-    {
-        if ($useShortFormat) {
-            return sprintf(
-                '%s: %s/%s/%s',
-                $this->name,
-                $this->weaponPower,
-                $this->jediFactor,
-                $this->strength
-            );
-        } else {
-            return sprintf(
-                '%s: w:%s, j:%s, s:%s',
-                $this->name,
-                $this->weaponPower,
-                $this->jediFactor,
-                $this->strength
-            );
-        }
-    }
+require __DIR__.'/lib/Ship.php';
+/**
+ * @param Ship $someShip
+ */
+function printShipSummary($someShip)
+{
+    echo 'Ship Name: '.$someShip->getName();
+    echo '<hr/>';
+    $someShip->sayHello();
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(false);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(true);
 }
 
 $myship = new Ship();
 $myship->name = 'TIE Starfighter';
 $myship->weaponPower = 10;
 
-echo 'Ship Name: '.$myship->getName();
+printShipSummary($myship);
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
 echo '<hr/>';
-$myship->sayHello();
+printShipSummary($otherShip);
 echo '<hr/>';
-echo $myship->getNameAndSpecs(false);
-echo '<hr/>';
-echo $myship->getNameAndSpecs(true);
+if ($myship->doesGivenShipHaveMoreStrength($otherShip)) {
+    echo $otherShip->name.' has more strength';
+} else {
+    echo $myship->name.' has more strength';
+}
